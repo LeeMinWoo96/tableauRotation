92fe1a2b7ebd47c886a37038c9903b5564506592
unknown <leeminwoo96@github.com>
2024-08-05 18:07:49 +0900

    cached rotation 추가

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 태블로 Embedded Views Auto Rotation
 
 V. 24.08.05  
-Tableau Ticket을 사용하여 구현되어 SameSite 이슈 떄문에 FireFox 기반 브라우저에만 작동   
+* Tableau Ticket을 사용하여 구현되어 SameSite 이슈 떄문에 FireFox 기반 브라우저에만 작동   
 [1. SameSite Issue 란?](https://www.codeit.kr/tutorials/94/%EC%BF%A0%ED%82%A4%EC%9D%98%20SameSite%20%EC%98%B5%EC%85%98%EC%9D%B4%EB%9E%80%3F)   
 [2. Tableau SameSite Issue](https://help.salesforce.com/s/articleView?id=001472205&type=1)
-
-
 
 * 토큰 발급 요청 (태블로 서버 URL) 과 Node 서버의 URL 이 일치하면 SameSite 무관   
 * 즉  태블로 서버가 공인망이면, Node 서버도 동일한 서버의 공인망이여야함, 만약 노트북 개발환경애서 해야한다면, https 설정과 SameSite=None 가 필수   
 * 태블로에서 SameSite=None 설정 가능 유무는 확인 필요
+
+------
+
+V. 24.08.06   
+* Cached Rotation 서비스 추가 --> 초기 로딩 후 지연 X(한시간 마다 재로당)   
+* but 크롬 정책으로 전체화면 간 자동 전환을 지원하지 않아, F11 로 수동 전체화면 진입 필요.   
+* 접속 url : http://{서버IP}/cache    
+
--- server/routes.js
+++ server/routes.js
@@ -5,12 +5,14 @@ import getTableauToken from '../scripts/getToken.js';
 const router = express.Router();
 
 const urls = [
-    // 'https://public.tableau.com/views/Superstore_embedded_800x800/Overview',
-    'http://121.254.201.144/trusted/:token/views/POS_NEW_2_17158490567760/POS?:embed=yes&:iid=2',
+    // 'https://public.tableau.com/views/Superstore_embedded_800x800/Overview?:language=ko-KR&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link',
+    'http://10.0.50.185/trusted/:token/views/POS_NEW_2_17158490567760/POS?:embed=yes&:showVizHome=no&:toolbar=no',
     // 'http://121.254.201.144/trusted/:token/t/2d8bafac-9006-4c28-ba08-28b3c02c9c41/views/POS_NEW_2_17158490567760/POS?:iid=2',
     // 'http://121.254.201.144/trusted/:token/t/121.254.201.144/views/POS_NEW_2_17158490567760/POS?:iid=2'
     // 'http://121.254.201.144/trusted/:token/t/views/2d8bafac-9006-4c28-ba08-28b3c02c9c41/POS_NEW_2_17158490567760/POS?:iid=2'
-    'http://121.254.201.144/trusted/:token/views/_BR_/BR?:embed=yes',
+    'http://10.0.50.185/trusted/:token/views/_BR_/BR?:embed=yes&:showVizHome=no&:toolbar=no',
+    'http://10.0.50.185/trusted/:token/views/_DD_/DD?:embed=yes&:showVizHome=no&:toolbar=no',
+    'http://10.0.50.185/trusted/:token/views/2/BR?:embed=yes&:showVizHome=no&:toolbar=no'
     // 'http://121.254.201.144/trusted/:token/views/_BR_/BR?:showVizHome=no&:embed=yes'
     // 'http://121.254.201.142/views/_DD_/DD?:showVizHome=no&:embed=true',
     // 'http://121.254.201.142/views/2/BR?:showVizHome=no&:embed=true'
@@ -51,5 +53,32 @@ router.get('/rotate', async (req, res) => {
     console.log(url)
     res.json({ url });
 });
+
+
+
+router.get('/urls', async (req, res) => {
+    try {
+        const resolvedUrls = await Promise.all(urls.map(async url => {
+            const token = await getTableauToken();
+
+            if (token == -1) {
+                throw new Error('Failed to get Tableau token');
+            }
+
+            return url.replace(':token', token);
+        }));
+
+        res.json({ urls: resolvedUrls });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+router.get('/url-sizes', async (req, res) => {
+    console.log("size ",urls.length)
+    res.json({ size : urls.length });
+});
+    
 
 export default router;
